f0b9378b1743399f4617b5eed49434b1dadfec97
Edward Shao <edwardxzshao@gmail.com>
2024-12-29 14:25:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+a ultra-fast nlp poker solver  
+  
+the solver is cloned from wasm-postflop's solver engine.  
+https://github.com/b-inary/postflop-solver
+
---
+++ nlp/main.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client = OpenAI()
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": "write a haiku about ai"}
+    ]
+)
+print(completion.choices[0].message.content)
